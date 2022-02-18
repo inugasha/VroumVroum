4382e1e7978bb2d442a83ce340ada741a74e215f
Your Name <david-e.caudron@outlook.fr>
2022-02-18 16:23:03 +0100

    Update VehiculeController

--- Assets/Scripts/VehiculeController.cs
+++ Assets/Scripts/VehiculeController.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 [RequireComponent(typeof(VehiculeMotor))]
 public class VehiculeController : MonoBehaviour
 {
     private VehiculeMotor _motor;
+    private float movementX;
 
     public float motorTorque;
     public float brakeTorque;
+    public float rotation;
 
     private void Start()
     {
@@ -17,6 +21,17 @@ public class VehiculeController : MonoBehaviour
     {
         _motor.motorTorque = motorTorque;
         _motor.brakeTorque = brakeTorque;
-        _motor.steerAngle = 25f;
+        _motor.steerAngle = movementX * rotation;
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+    }
+
+    private void OnAcceleration(AxisControl inputValue)
+    {
+        Debug.Log(inputValue);
     }
 }
