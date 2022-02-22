48177681de452332f4dd6b7dbb5ad568b8d9fa3b
Your Name <david-e.caudron@outlook.fr>
2022-02-22 11:54:32 +0100

    des trucs

--- Assets/Scripts/VehiculeController.cs
+++ Assets/Scripts/VehiculeController.cs
@@ -9,7 +9,7 @@ public class VehiculeController : MonoBehaviour
     private float _movementX;
     private float _movementY;
 
-    [Header("Motor speed")]public float MotorTorque = 25f;
+    [Header("Motor speed")] public float MotorTorque = 25f;
     [Header("Motor braking")] public float BrakeTorque = 50f;
     [Header("Wheel rotation")] public float Rotation = 30f;
 
@@ -26,37 +26,23 @@ public class VehiculeController : MonoBehaviour
 
     private void CalculateTorque()
     {
-        //if (_movementY > 0.0f)
-        //{
-        //    _motor.MotorTorque = _movementY * MotorTorque;
-        //    _motor.BrakeTorque = 0.0f;
-        //}
-        //else
-        //{
-        //    //if (_movementY < 0.0f)
-        //    //{
-        //    //    _motor.BrakeTorque = (-_movementY) * MotorTorque * 10;
-        //    //}
-        //    //else
-        //    //{
-        //    //    _motor.BrakeTorque = MotorTorque;
-        //    //}
-
-        //    _motor.BrakeTorque = _movementY < 0.0f ? (-_movementY) * MotorTorque * 10 : MotorTorque;
-        //}
-
-
-        if(_movementY != 0.0f)
+        if (_movementY > 0.0f)
         {
-            //Accelère ou recule
             _motor.MotorTorque = _movementY * MotorTorque;
-            _motor.BrakeTorque = 0f;
+            _motor.BrakeTorque = 0.0f;
         }
         else
         {
-            //n'accelere pas
-            _motor.MotorTorque = 0f;
-            _motor.BrakeTorque = BrakeTorque;
+            //if (_movementY < 0.0f)
+            //{
+            //    _motor.BrakeTorque = (-_movementY) * MotorTorque * 10;
+            //}
+            //else
+            //{
+            //    _motor.BrakeTorque = MotorTorque;
+            //}
+
+            _motor.BrakeTorque = _movementY < 0.0f ? (-_movementY) * MotorTorque * 10 : MotorTorque;
         }
     }
 
