a277526432613d894ba582329d52850bf9054b95
inugasha <53877578+inugasha@users.noreply.github.com>
2022-02-25 15:11:08 +0100

    plauer ui update

--- Assets/Scripts/PlayerUi.cs
+++ Assets/Scripts/PlayerUi.cs
@@ -7,19 +7,27 @@ public class PlayerUi : MonoBehaviour
     [SerializeField] private Slider _slider;
     [SerializeField] private Gradient _gradient;
     [SerializeField] private Image _fill;
+    [SerializeField] private TMPro.TextMeshProUGUI _roundText;
 
     public void Setup(Vehicule vehicule)
     {
         vehicule.HPChangedDelegate += UpdateHP;
+        vehicule.Data.CurrentRoundChanged += UpdateCurrentRound;
 
         _maxHP = vehicule.MaxHP;
         _slider.maxValue = _maxHP;
         UpdateHP(vehicule.MaxHP);
+        UpdateCurrentRound(1);
     }
 
     private void UpdateHP(int amount)
     {
         _slider.value = amount;
         _fill.color = _gradient.Evaluate((float)amount/(float)_maxHP);
+    }
+
+    private void UpdateCurrentRound(object value)
+    {
+        _roundText.text = $"{(int)value} / {GameManager.Instance.MaxRound}";
     }
 }
